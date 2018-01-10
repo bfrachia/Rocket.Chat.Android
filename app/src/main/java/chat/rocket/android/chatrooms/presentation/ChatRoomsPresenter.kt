67781b9307e72e06f888ad4f8cd6d41c1946e7a5
Lucio Maciel <luciofm@gmail.com>
2018-01-10 18:32:22 -0300

    Listen to Room and Subscription changes

--- app/src/main/java/chat/rocket/android/chatrooms/presentation/ChatRoomsPresenter.kt
+++ app/src/main/java/chat/rocket/android/chatrooms/presentation/ChatRoomsPresenter.kt
@@ -5,26 +5,52 @@ import chat.rocket.android.server.domain.GetCurrentServerInteractor
 import chat.rocket.android.server.infraestructure.RocketChatClientFactory
 import chat.rocket.android.util.launchUI
 import chat.rocket.core.RocketChatClient
+import chat.rocket.core.internal.model.Subscription
+import chat.rocket.core.internal.realtime.*
 import chat.rocket.core.internal.rest.chatRooms
 import chat.rocket.core.model.ChatRoom
+import chat.rocket.core.model.Room
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 class ChatRoomsPresenter @Inject constructor(private val view: ChatRoomsView,
                                              private val strategy: CancelStrategy,
                                              private val serverInteractor: GetCurrentServerInteractor,
                                              private val factory: RocketChatClientFactory) {
-    lateinit var client: RocketChatClient
+    private val client: RocketChatClient = factory.create(serverInteractor.get()!!)
 
-    fun chatRooms() {
-        // TODO - check for current server
-        client = factory.create(serverInteractor.get()!!)
+    // FIXME - cache rooms on memory while we don't have a Database...
+    private var chatRooms = ArrayList<ChatRoom>()
+    private var reloadJob: Job? = null
+
+    fun loadChatRooms() {
         launchUI(strategy) {
             view.showLoading()
-            val chatRooms = client.chatRooms().update
-            val openChatRooms = getOpenChatRooms(chatRooms)
-            val sortedOpenChatRooms = sortChatRooms(openChatRooms)
-            view.showChatRooms(sortedOpenChatRooms.toMutableList())
+            loadRooms()
+            updateRooms()
             view.hideLoading()
+            subscribeRoomUpdates()
+        }
+    }
+
+    private suspend fun loadRooms() {
+        val rooms = client.chatRooms().update
+        chatRooms.clear()
+        chatRooms.addAll(rooms)
+    }
+
+    private fun sortRooms(): List<ChatRoom> {
+        val openChatRooms = getOpenChatRooms(chatRooms)
+        return sortChatRooms(openChatRooms)
+    }
+
+    private fun updateRooms() {
+        launch {
+            view.updateChatRooms(sortRooms().toMutableList())
         }
     }
 
@@ -35,6 +61,153 @@ class ChatRoomsPresenter @Inject constructor(private val view: ChatRoomsView,
     private fun sortChatRooms(chatRooms: List<ChatRoom>): List<ChatRoom> {
         return chatRooms.sortedByDescending {
             chatRoom -> chatRoom.lastMessage?.timestamp
+        }
+    }
+
+    // TODO - Temporary stuff, remove when adding DB support
+    private suspend fun subscribeRoomUpdates() {
+        launch(CommonPool + strategy.jobs) {
+            for (status in client.statusChannel) {
+                Timber.d("Changing status to: $status")
+                when (status) {
+                    State.Authenticating -> Timber.d("Authenticating")
+                    State.Connected -> {
+                        Timber.d("Connected")
+                        client.subscribeSubscriptions()
+                        client.subscribeRooms()
+                    }
+                }
+            }
+            Timber.d("Done on statusChannel")
+        }
+
+        client.connect()
+
+        launch(CommonPool + strategy.jobs) {
+            for (message in client.roomsChannel) {
+                Timber.d("Got message: $message")
+                updateRoom(message)
+            }
+        }
+
+        launch(CommonPool + strategy.jobs) {
+            for (message in client.subscriptionsChannel) {
+                Timber.d("Got message: $message")
+                updateSubscription(message)
+            }
+        }
+    }
+
+    private fun updateRoom(message: StreamMessage<Room>) {
+        launchUI(strategy) {
+            when (message.type) {
+                Type.Removed -> {
+                    removeRoom(message.data.id)
+                }
+                Type.Updated -> {
+                    updateRoom(message.data)
+                }
+                Type.Inserted -> {
+                    // On insertion, just get all chatrooms again, since we can't create one just
+                    // from a Room
+                    reloadRooms()
+                }
+            }
+
+            updateRooms()
+        }
+    }
+
+    private fun updateSubscription(message: StreamMessage<Subscription>) {
+        launchUI(strategy) {
+            when (message.type) {
+                Type.Removed -> {
+                    removeRoom(message.data.roomId)
+                }
+                Type.Updated -> {
+                    updateSubscription(message.data)
+                }
+                Type.Inserted -> {
+                    // On insertion, just get all chatrooms again, since we can't create one just
+                    // from a Subscription
+                    reloadRooms()
+                }
+            }
+
+            updateRooms()
+        }
+    }
+
+    private suspend fun reloadRooms() {
+        reloadJob?.cancel()
+
+        reloadJob = launch(CommonPool + strategy.jobs) {
+            delay(1000)
+            Timber.d("reloading rooms after wait")
+            loadRooms()
+        }
+        reloadJob?.join()
+    }
+
+    // Update a ChatRoom with a Room information
+    private fun updateRoom(room: Room) {
+        val chatRoom = chatRooms.find { chatRoom -> chatRoom.id == room.id }
+        chatRoom?.apply {
+            val newRoom = ChatRoom(room.id,
+                    room.type,
+                    room.user ?: user,
+                    room.name ?: name,
+                    room.fullName ?: fullName,
+                    room.readonly,
+                    room.updatedAt ?: updatedAt,
+                    timestamp,
+                    lastModified,
+                    room.topic,
+                    room.announcement,
+                    default,
+                    open,
+                    alert,
+                    unread,
+                    userMenstions,
+                    groupMentions,
+                    room.lastMessage,
+                    client)
+            removeRoom(room.id)
+            chatRooms.add(newRoom)
+        }
+    }
+
+    // Update a ChatRoom with a Subscription information
+    private fun updateSubscription(subscription: Subscription) {
+        val chatRoom = chatRooms.find { chatRoom -> chatRoom.id == subscription.roomId }
+        chatRoom?.apply {
+            val newRoom = ChatRoom(subscription.roomId,
+                    subscription.type,
+                    subscription.user ?: user,
+                    subscription.name,
+                    subscription.fullName ?: fullName,
+                    subscription.readonly ?: readonly,
+                    subscription.updatedAt ?: updatedAt,
+                    subscription.timestamp ?: timestamp,
+                    subscription.lastModified ?: lastModified,
+                    topic,
+                    announcement,
+                    subscription.isDefault,
+                    subscription.open,
+                    subscription.alert,
+                    subscription.unread,
+                    subscription.userMentions,
+                    subscription.groupMentions,
+                    lastMessage,
+                    client)
+            removeRoom(subscription.roomId)
+            chatRooms.add(newRoom)
+        }
+    }
+
+    private fun removeRoom(id: String) {
+        synchronized(chatRooms) {
+            chatRooms.removeAll { chatRoom -> chatRoom.id == id }
         }
     }
 }
--- app/src/main/java/chat/rocket/android/chatrooms/ui/ChatRoomsFragment.kt
+++ app/src/main/java/chat/rocket/android/chatrooms/ui/ChatRoomsFragment.kt
@@ -2,6 +2,8 @@ package chat.rocket.android.chatrooms.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.util.DiffUtil
+import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +16,10 @@ import chat.rocket.android.widget.DividerItemDecoration
 import chat.rocket.core.model.ChatRoom
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_chat_rooms.*
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 import javax.inject.Inject
 
 class ChatRoomsFragment : Fragment(), ChatRoomsView {
@@ -32,14 +38,26 @@ class ChatRoomsFragment : Fragment(), ChatRoomsView {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        presenter.chatRooms()
-    }
-
-    override fun showChatRooms(dataSet: MutableList<ChatRoom>) {
         activity?.apply {
             recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
             recycler_view.addItemDecoration(DividerItemDecoration(this, 144, 32))
-            recycler_view.adapter = ChatRoomsAdapter(dataSet, this)
+            recycler_view.adapter = ChatRoomsAdapter(this)
+            recycler_view.itemAnimator = DefaultItemAnimator()
+        }
+        presenter.loadChatRooms()
+    }
+
+    override suspend fun updateChatRooms(newDataSet: MutableList<ChatRoom>) {
+        activity.apply {
+            launch(UI) {
+                val adapter = recycler_view.adapter as ChatRoomsAdapter
+                val diff = async(CommonPool) {
+                    DiffUtil.calculateDiff(RoomsDiffCallback(adapter.dataSet, newDataSet))
+                }.await()
+
+                adapter.updateRooms(newDataSet)
+                diff.dispatchUpdatesTo(adapter)
+            }
         }
     }
 
@@ -50,4 +68,27 @@ class ChatRoomsFragment : Fragment(), ChatRoomsView {
     override fun showMessage(message: String) = Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
 
     override fun showGenericErrorMessage() = showMessage(getString(R.string.msg_generic_error))
+
+    class RoomsDiffCallback(private val oldRooms: List<ChatRoom>,
+                            private val newRooms: List<ChatRoom>) : DiffUtil.Callback() {
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldRooms[oldItemPosition].id == newRooms[newItemPosition].id
+        }
+
+        override fun getOldListSize(): Int {
+            return oldRooms.size
+        }
+
+        override fun getNewListSize(): Int {
+            return newRooms.size
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldRooms[oldItemPosition].updatedAt == newRooms[newItemPosition].updatedAt
+        }
+
+        override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+            return newRooms[newItemPosition]
+        }
+    }
 }
